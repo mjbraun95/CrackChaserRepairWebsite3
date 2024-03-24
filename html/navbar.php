a00206643d70cdd3f1d44635d700497dcb35d133
Matthew Braun <43424316+mjbraun95@users.noreply.github.com>
2024-03-24 14:46:45 -0600

    Removed About section

--- html/navbar.php
+++ html/navbar.php
@@ -5,7 +5,7 @@
         </div>
         <div class="navbar-right">
             <a href="index.php" <?php if ($currentPage === 'Home') {echo 'class="active"';} ?>>Home</a>
-            <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a>
+            <!-- <a href="about.php" <?php if ($currentPage === 'About') {echo 'class="active"';} ?>>About</a> -->
             <a href="services.php" <?php if ($currentPage === 'Services') {echo 'class="active"';} ?>>Services</a>
             <a href="contact.php" <?php if ($currentPage === 'Contact') {echo 'class="active"';} ?>>Contact</a>
             <a href="beforeandafter.php" <?php if ($currentPage === 'Before & After') {echo 'class="active"';} ?>>Before & After</a>
